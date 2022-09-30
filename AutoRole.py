64653f51dd133b539e540356eef489cdac778dea
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-09-30 09:14:23 +0800

    整合config

--- AutoRole.py
+++ AutoRole.py
@@ -1,18 +1,14 @@
-#導入 Discord.py
 from email import message
 from discord.utils import get
 import discord
-
-import RoleID
-import RoleName
-import TOKEN
+import config
 
 intents=discord.Intents.default()
 intents=discord.Intents().all()
 intents.message_content = True
 intents.members = True
 
-#client 是我們與 Discord 連結的橋樑
+#bot=discord.Client 是我們與 Discord 連結的橋樑
 bot = discord.Client(intents=intents)
 
 #調用 event 函式庫
@@ -30,13 +26,13 @@ async def on_ready():
 #當有成員加入（DC群）時
 async def on_member_join(member):
     #以下兩個請挑其中一個使用，不用的註解掉即可
-    #給予加入DC群的成員 RoleID.id 的身分組
-    role = discord.utils.get(member.guild.roles, id=RoleID.id)
-    #給予加入DC群的成員 RoleName.name 的身分組
-    #role = discord.utils.get(member.guild.roles, id=RoleName.name)
+    #給予加入DC群的成員 config.id 的身分組
+    role = discord.utils.get(member.guild.roles, id=config.id)
+    #給予加入DC群的成員 config.name 的身分組
+    #role = discord.utils.get(member.guild.roles, id=config.name)
 
     await member.add_roles(role)
     #當然這也可以打 await message.channel.send("歡迎加入") 等做出加入DC時發送的訊息
     #await message.channel.send("歡迎加入(XXX)DC群")
 
-bot.run(TOKEN.TOKEN) #TOKEN 在 Discord Developer 的 BOT 頁面
+bot.run(config.TOKEN) #TOKEN 在 Discord Developer 的 BOT 頁面
