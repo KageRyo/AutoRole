4ffb3f343d9fb9a95045c55aac27d1145579d7f0
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-09-30 09:23:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #在這裡設定AutoRole的身分組名稱，別忘了在DC上先創建好身分組
 name="your role name (english only)"
 #在這裡設定AutoRole的身分組ID，別忘了在DC上先創建好身分組
-id=your discord server role id (find it by developer mode)
+id=#your discord server role id (find it by developer mode)
 #TOKEN請填入自己機器人的TOKEN，切記不要將此上傳到 GitHub 的 Public Repo，會被吃掉
 TOKEN=''
