601db7000e176c56434e6606ef10262728dd7875
KageRyo <36478298+KageRyo@users.noreply.github.com>
2022-09-30 09:12:46 +0800

    Update and rename RoleName.py to config.py

--- RoleName.py
+++
@@ -1,2 +0,0 @@
-#在這裡設定AutoRole的身分組名稱，別忘了在DC上先創建好身分組
-name="your role name (english only)"
---
+++ config.py
@@ -0,0 +1,4 @@
+#在這裡設定AutoRole的身分組名稱，別忘了在DC上先創建好身分組
+name="your role name (english only)"
+#在這裡設定AutoRole的身分組ID，別忘了在DC上先創建好身分組
+id=your discord server role id (find it by developer mode)
